d1539e9c605886b49d7dddb9703fade65ab05e48
Gibby <john.gibby@localmed.com>
2015-12-07 12:04:04 -0600

    integrating gulp to project

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
-	gulp.src('scss/kube.scss')
+	gulp.src('/scss/tgggrid.scss')
 	  	.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('./css/'));
 });
 
 gulp.task('default',function() {
-    gulp.watch('scss/kube.scss',['styles']);
+    gulp.watch('/scss/tgggrid.scss',['styles']);
 });
