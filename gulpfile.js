5d7a79e3e3f1cfb88d4bd69f94cb8a96997a6076
Gibby <john.gibby@localmed.com>
2015-12-07 20:59:53 -0600

    added gulp file, bump version to 0.0.2

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,50 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
+    autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('styles', function() {
-	gulp.src('/scss/tgggrid.scss')
-	  	.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./css/'));
+gulp.task('tgggrid', function() {
+  gulp.src('./scss/_tgggrid.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer(
+    { cascade: true }
+  ))
+  .pipe(gulp.dest('./css/'))
 });
 
+gulp.task('tgggridMinified', function() {
+  gulp.src('./scss/_tgggrid.scss')
+  .pipe(sass({
+    outputStyle: 'compressed'
+  }).on('error', sass.logError))
+  .pipe(autoprefixer(
+    { cascade: false }
+  ))
+  .pipe(rename('tgggrid.min.css'))
+  .pipe(gulp.dest('./css/'))
+});
+
+gulp.task('styles', function() {
+  gulp.src('./scss/styles.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer(
+    { cascade: true }
+  ))
+  .pipe(gulp.dest('./css/'))
+})
+
+gulp.task('stylesMin', function() {
+  gulp.src('./scss/styles.scss')
+  .pipe(sass({
+    outputStyle: 'compressed'
+  }).on('error', sass.logError))
+  .pipe(autoprefixer(
+    { cascade: true }
+  ))
+  .pipe(rename('styles.min.css'))
+  .pipe(gulp.dest('./css/'))
+})
+
 gulp.task('default',function() {
-    gulp.watch('/scss/tgggrid.scss',['styles']);
+  gulp.watch('./scss/styles.scss',['styles','stylesMin']);
 });
