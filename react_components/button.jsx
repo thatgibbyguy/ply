de26b0814288abb7e8874ba1a5841d59a7dea55c
Gibby <john.gibby@localmed.com>
2016-01-20 20:02:34 -0600

    added react components, completed fancy select

---
+++ react_components/button.jsx
@@ -0,0 +1,70 @@
+const types = ['button', 'reset', 'span', 'submit']
+
+Button = React.createClass({
+
+	propTypes: {
+		type: React.PropTypes.oneOf(types),
+		buttonText: React.PropTypes.string,
+		active: React.PropTypes.bool,
+    	disabled: React.PropTypes.bool,
+    	componentClass: React.PropTypes.string,
+    	href: React.PropTypes.string,
+    	target: React.PropTypes.string,
+    	onClick: React.PropTypes.func
+	},
+
+	getDefaultProps() {
+		return {
+			disabled: false
+		}
+	},
+
+	onClick(event) {
+		event.preventDefault();
+		const {onClick, isDisabled} = this.props;
+		if (onClick && !isDisabled) {
+			onClick(event);
+		}
+	},
+
+	render() {
+		const {type, buttonText, active, disabled, componentClass, href, target, onClick} = this.props;
+		const buttonClass = `btn ${componentClass}`;
+
+		if (type === 'button') {
+			return (
+				<button className={buttonClass} disabled={disabled}>
+					{buttonText}
+				</button>
+			)
+		}
+
+		else if (type === 'span') {
+			return (
+				<span className={buttonClass} disabled={disabled}>
+					{buttonText}
+				</span>
+			)
+		}
+
+		else if (type === 'reset') {
+			return (
+				<input type="reset" value={buttonText} className={buttonClass} disabled={disabled} />
+			)
+		}
+
+		else if (type === 'submit') {
+			return (
+				<input type="submit" value={buttonText} className={buttonClass} disabled={disabled} />
+			)
+		}
+
+		else {
+			return (
+				<a href={href} className={buttonClass} disabled={disabled} >{buttonText}</a>
+			)
+		}
+
+	}
+
+})
